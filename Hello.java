af967328af7d329bd2c9074e6db80f53765551c1
kong <kong@gmail.com>
2020-08-20 11:25:00 +0700

    update text to sofewrae engineer

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
 public class Hello{
 	public static void main(String[ ] args){
 		System.out.println("Hello Java");
-		Hello1 h1 = new Hello1();
-		Hello2 h2 = new Hello2();
-		Hello3 h3 = new Hello3();
-		System.out.println(h1);
-		System.out.println(h2);
-		System.out.println(h3);
+		
 
 	}
 }
